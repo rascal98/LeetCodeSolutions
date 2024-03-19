9472d602780aab1a760cc86b0e48847993d655aa
Umut Eskitoğlu <43891600+rascal98@users.noreply.github.com>
2024-03-19 21:48:36 +0300

    find the index

--- LeetCodeSolutions/FindTheIndexOfString.cs
+++ LeetCodeSolutions/FindTheIndexOfString.cs
@@ -1,6 +1,9 @@
 namespace LeetCodeSolutions;
 
+//LeetCode 28. Find the Index of the First Occurrence in a String
 public class FindTheIndexOfString
 {
-    
+    public int StrStr(string haystack, string needle) {
+        return haystack.IndexOf(needle);
+    }
 }
